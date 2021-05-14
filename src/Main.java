507ad993ca1df6d08a63c58c57e6983beb60427e
Vago Grigoryan <Vago_1989@mail.ru>
2021-05-14 21:59:26 +0300

    первый коммит

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 7500;
+        int priceOf1Mile = 20;
+        int totalMiles = price / priceOf1Mile;
+
+        System.out.println(totalMiles);
+    }
+}
+
